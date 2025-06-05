e2d8a48548431eff6b8e476bcaa27dc82bde6f58
hahamik <dlapdl0615@gmail.com>
2025-06-05 11:12:26 +0900

    1

---
+++ lib/ui/pages/auth/join_page/widgets/join_form.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/_core/constants/size.dart';
+import 'package:flutter_blog/ui/widgets/custom_auth_text_form_field.dart';
+import 'package:flutter_blog/ui/widgets/custom_elavated_button.dart';
+import 'package:flutter_blog/ui/widgets/custom_text_button.dart';
+
+class JoinForm extends StatelessWidget {
+  const JoinForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          CustomAuthTextFormField(
+            text: "Username",
+          ),
+          const SizedBox(height: mediumGap),
+          CustomAuthTextFormField(
+            text: "Email",
+          ),
+          const SizedBox(height: mediumGap),
+          CustomAuthTextFormField(
+            text: "Password",
+            obscureText: true,
+          ),
+          const SizedBox(height: largeGap),
+          CustomElevatedButton(
+            text: "회원가입",
+            click: () {},
+          ),
+          CustomTextButton(
+            text: "로그인 페이지로 이동",
+            click: () {
+              Navigator.pushNamed(context, "/login");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/widgets/custom_auth_text_form_field.dart
+++ lib/ui/widgets/custom_auth_text_form_field.dart
@@ -4,10 +4,17 @@ import 'package:flutter_blog/_core/constants/size.dart';
 class CustomAuthTextFormField extends StatelessWidget {
   final String text;
   final bool obscureText;
+  final String? value;
+  final String? errorText;
+  final Function(String)? onChanged;
 
   const CustomAuthTextFormField({
+    super.key,
     required this.text,
     this.obscureText = false,
+    this.value,
+    this.errorText,
+    this.onChanged,
   });
 
   @override
@@ -18,23 +25,22 @@ class CustomAuthTextFormField extends StatelessWidget {
         Text(text),
         const SizedBox(height: smallGap),
         TextFormField(
+          initialValue: value,
           obscureText: obscureText,
+          onChanged: onChanged,
           decoration: InputDecoration(
             hintText: "Enter $text",
+            errorText: errorText,
             enabledBorder: OutlineInputBorder(
-              // 3. 기본 TextFormField 디자인
               borderRadius: BorderRadius.circular(20),
             ),
             focusedBorder: OutlineInputBorder(
-              // 4. 손가락 터치시 TextFormField 디자인
               borderRadius: BorderRadius.circular(20),
             ),
             errorBorder: OutlineInputBorder(
-              // 5. 에러발생시 TextFormField 디자인
               borderRadius: BorderRadius.circular(20),
             ),
             focusedErrorBorder: OutlineInputBorder(
-              // 5. 에러가 발생 후 손가락을 터치했을 때 TextFormField 디자인
               borderRadius: BorderRadius.circular(20),
             ),
           ),
